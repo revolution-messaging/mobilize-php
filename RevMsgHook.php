2c9b3bb93833b96a17e0f3f1095a938f7a725059
Rich Ranallo <zincoxide@gmail.com>
2013-11-15 11:25:54 -0500

    Added RevMsgHook.php

---
+++ RevMsgHook.php
@@ -0,0 +1,90 @@
+<?php
+class RevMsgHook {
+	protected $endSession = true;
+	protected $response = null;
+	protected $format = null;
+	protected $method = null;
+	protected $inputs = array(
+		'strippedText' 	=> null,
+		'msisdn'       	=> null,
+		'mobileText'  	=> null,
+		'keywordName' 	=> null,
+		'keywordId'   	=> null,
+		'shortCode'   	=> null,
+		'subscriberId' => null,                  
+		'metadataId'   => null,
+		'oldValue'     => array(),
+		'newValue'     => null
+		);
+	
+	
+	public function __construct($format='xml',$method='post'){
+		$this->format = $format;
+		$this->method = $method;
+		$this->retreiveinputs($this->method);
+	}
+	
+	public function changemsg($msg){
+		$this->response = $msg;
+	}
+	
+	public function changeend($end=true) {
+		$this->endSession = $end;
+	}
+	
+	public function getEndSession (){
+		return $this->endSession;
+	}
+	
+	public function getResponse (){
+		return $this->response;
+	}
+	
+	public function stripText(){
+		$this->inputs['strippedText'] = preg_replace('/^('.$this->inputs['keywordName'].'\s+)/i',null,$this->inputs['mobileText']);
+	}
+	
+	public function retreiveinputs($method){
+		if($method=='get'){
+			$d=$_GET;
+		}elseif($method=='post'){
+			file_get_contents('php://input');
+			if($this->format=='xml'){
+				$d = new SimpleXMLElement($d);
+			}elseif($this->format=='json'){
+				$d = json_decode($d,true);
+			}
+		}
+		foreach($d as $var => $val){
+			if(in_array($var,array_keys($this->inputs)))
+				$this->inputs[$var] = $val;
+			}
+		$this->stripText();
+	}
+	
+	public function getinputs(){
+		return $this->inputs;
+	}
+	
+	public function setInput($var,$val){
+		if(in_array($var,array_keys($this->inputs))){
+			$this->inputs[$var] = $val;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public function outputDC() {
+		switch($this->format){
+		case 'xml':
+			return "<dynamiccontent><endSession>".$this->getEndSession(true)."</endSession><response>".htmlspecialchars($this->ttruncat($this->response,160))."</response></dynamiccontent>";
+		case 'json':
+			return json_encode(array(
+				'endSession'=>$this->getEndSession(),
+				'response'=>$this->ttruncat($this->response,160)
+			));
+		}
+	}
+}
+?>
