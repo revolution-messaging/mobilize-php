9995a6cf6a8ef3ceda0a8d5e9ca4a6a9d682d015
Rich Ranallo <zincoxide@gmail.com>
2014-02-17 19:06:29 -0500

    added inline documentation, reqorganized object classes

--- mobilize.object.php
+++ mobilize.object.php
@@ -4,6 +4,7 @@ use Guzzle\Http\Client;
 use Guzzle\Plugin\Cookie\CookiePlugin;
 use Guzzle\Plugin\Cookie\CookieJar\ArrayCookieJar;
 use Revmsg\Mobilize\Model;
+
 interface object			{
 	public function isDirty		();
 	public function delete          ($session,$version);
@@ -116,6 +117,71 @@ class platformObject implements object	{
 	}
 }
 
+//creating an authentication session is the first step in using the API at all. Instance of this class needs to be passed to all CRUD methods of other objects. 
+class authentication extends platformObject	{
+	public $client		=	null;
+	public $model		=	null;
+	protected $impersonate	=	null;
+	protected $urls		=	array(
+		'v1'		=>	array(
+			'retrieve'	=>	'v1/authenticate/whoami',
+			'create'	=>	'v1/authenticate',
+			'delete'	=>	'v1/authenticate/logout'
+			),
+		'v2'		=>	array(
+			'create'	=>	'',
+			'update'	=>	''
+			)
+		);
+	
+	public function create		($session=null,$version='v1'){
+		$request	=	$this-> client-> post($this-> urls[$version]['create'],null,json_encode($this-> model-> getVariables()));
+		$response	=	$request-> send();
+	}
+	
+	public function retrieve	($id=null,$session=null,$version='v1'){
+		if (!isset($this-> urls['v1']['retrieve'])){
+			throw new Exception('object cannot be retrieved');
+		}else{
+			$request	=	$this-> client-> get($this-> urls[$version]['retrieve']);
+			$response	=	$request-> send();
+			if($response-> getStatusCode() < 400){
+				return json_decode($response-> getBody(),true);
+			}else{
+				throw new Exception($response-> getStatusCode().': '.$response-> getBody());
+				return false;
+			}
+		}
+	}
+	 public function __invoke(){
+		if(!isset($this-> client) || empty($this-> client)){
+			throw new Exception('No session active. Reauthenticate.');
+		}else{
+			return($this-> client);
+		}
+	}
+	
+	public function __construct($signifier=array()){
+		$this-> model		=	new model\authentication;
+		if(!empty($signifier) && isset($signifier)){
+			$this-> model-> setVariables($signifier);
+		}
+		$this-> client	=	new Client('http://revolutionmsg.com/api',array(
+			'request.options'	=>	array(
+				'headers'	=>	array(
+					'Accept'	=>	'application/json',
+					'Content-Type'	=>	'application/json'
+					)
+				)
+			)
+		);
+		$this-> client-> setUserAgent('Revmsg/Mobilize');
+		$cookiePlugin = new CookiePlugin(new ArrayCookieJar());
+		$this-> client-> addSubscriber($cookiePlugin);
+	}
+	
+}
+
 class impersonate extends platformObject {
 	protected $scheme	=	'Revmsg\Mobilize\Model\impersonate';
 	protected $urls		=	array(
@@ -183,84 +249,5 @@ class filter extends platformObject	{
 		);
 }
 
-class authentication extends platformObject	{
-	public $client		=	null;
-	public $model		=	null;
-	protected $impersonate	=	null;
-	protected $urls		=	array(
-		'v1'		=>	array(
-			'retrieve'	=>	'v1/authenticate/whoami',
-			'create'	=>	'v1/authenticate',
-			'delete'	=>	'v1/authenticate/logout'
-			),
-		'v2'		=>	array(
-			'create'	=>	'',
-			'update'	=>	''
-			)
-		);
-	
-	public function create		($session=null,$version='v1'){
-		$request	=	$this-> client-> post($this-> urls[$version]['create'],null,json_encode($this-> model-> getVariables()));
-		$response	=	$request-> send();
-	}
-	
-	public function retrieve	($id=null,$session=null,$version='v1'){
-		if (!isset($this-> urls['v1']['retrieve'])){
-			throw new Exception('object cannot be retrieved');
-		}else{
-			$request	=	$this-> client-> get($this-> urls[$version]['retrieve']);
-			$response	=	$request-> send();
-			if($response-> getStatusCode() < 400){
-				return json_decode($response-> getBody(),true);
-			}else{
-				throw new Exception($response-> getStatusCode().': '.$response-> getBody());
-				return false;
-			}
-		}
-	}
-	
-	/*public function update		($signifier, $version='v1'){
-		if (!isset($this->urls['v1']['update'])){
-			throw new \Exception('object cannot be updated');
-		}else{
-			$request	=	$session()-> post($this-> urls['v1']['update'].'/'.$signifier);
-			$response	=	$request-> send();
-			if($response-> getStatusCode() < 400){
-				$this-> model-> isDirty	=	false;
-				return $this;
-			}else{
-				throw new Exception($response-> getStatusCode().': '.$response-> getBody());
-				return false;
-			}
-		}
-	}*/
-	
-	public function __invoke(){
-		if(!isset($this-> client) || empty($this-> client)){
-			throw new Exception('No session active. Reauthenticate.');
-		}else{
-			return($this-> client);
-		}
-	}
-	
-	public function __construct($signifier=array()){
-		$this-> model		=	new model\authentication;
-		if(!empty($signifier) && isset($signifier)){
-			$this-> model-> setVariables($signifier);
-		}
-		$this-> client	=	new Client('http://revolutionmsg.com/api',array(
-			'request.options'	=>	array(
-				'headers'	=>	array(
-					'Accept'	=>	'application/json',
-					'Content-Type'	=>	'application/json'
-					)
-				)
-			)
-		);
-		$this-> client-> setUserAgent('Revmsg/Mobilize');
-		$cookiePlugin = new CookiePlugin(new ArrayCookieJar());
-		$this-> client-> addSubscriber($cookiePlugin);
-	}
-	
-}
+
 ?>
